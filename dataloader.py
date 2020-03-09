a82c9879bdc1e0bdcc953e62d3af7f5b67e5a004
AkbarShah96 <shahak@student.ethz.ch>
2020-03-09 16:57:13 +0100

    Updates

--- dataloader.py
+++ dataloader.py
@@ -8,6 +8,86 @@ import os
 import torch
 import numpy as np
 import torchvision
+from PIL import Image
+import matplotlib.pyplot as plt
+from skimage import io, transform
+from torchvision import transforms
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+"""
+Note: In order to use this file, you need to organise your data in the following structure:
+    CMP_Facades:
+        rgb
+            cmp_b0001
+            cmp_b0002
+            cmp_b0003
+        semantics
+            cmp_b0001
+            cmp_b0002
+            cmp_b0003
+"""
+
+class facades(Dataset):
+    def __init__(self,
+                 root_dir = "C:\\Users\\akbar\\PycharmProjects\\CMP_Facades",
+                 img_height = 256,
+                 img_width = 256,
+                 transform = transforms.ToTensor()
+                 ):
+        self.root_dir = root_dir
+        self.img_height = img_height
+        self.img_width = img_width
+        self.transform = transform
+        self.load_data()
+
+    def load_data(self):
+        self.rgb_files = []
+        self.semantics_files = []
+        "Join path of rgb directory and semantic, \\ to complete the path for loop below."
+        rgb_path = os.path.join(self.root_dir,'rgb\\')
+        semantics_path = os.path.join(self.root_dir,'semantics\\')
+
+        "Collect Files"
+
+        for rgb_file in os.listdir(rgb_path):
+            self.rgb_files.append(rgb_path + rgb_file)
+        self.rgb_files = sorted(self.rgb_files)
+
+        for semantic_file in os.listdir(semantics_path):
+            self.semantics_files.append(semantics_path + semantic_file)
+        self.semantics_files = sorted(self.semantics_files)
+
+    def resize_randomcrop(self, rgb, semantic):
+        torchvision.transforms.Resize(self.img_width,interpolation=2)(rgb)
+        torchvision.transforms.Resize(self.img_width, interpolation=2)(semantic)
+        return rgb, semantic
+
+    def __len__(self):
+        return len(self.rgb_files)
+
+    def __getitem__(self, item):
+        rgb_item = self.rgb_files[item]
+        semantic_item = self.semantics_files[item]
+        sample = {}
+
+        rgb = Image.open(rgb_item)
+        semantic = Image.open(semantic_item)
+        sample['rgb'], sample['semantic'] = self.resize_randomcrop(rgb, semantic)
+        sample['rgb'] = self.transform(sample['rgb'])
+        sample['semantic'] = self.transform(sample['semantic'])
+
+        return sample
 
 
+"Here I test the dataset to make sure everything is loading correctly"
+if __name__ == '__main__':
+    dataset = facades()
+    trainloader = DataLoader(dataset,
+                             batch_size=1,
+                             shuffle=False)
+    for i,batch in enumerate(trainloader):
+        rgb = torchvision.utils.make_grid(batch['rgb'])
+        rgb = rgb.numpy().transpose((1, 2, 0))
+        plt.imshow(rgb.astype('uint8'))
+        plt.pause(0.001)
--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 
 class down_sample(nn.Module):
-    def __init__(self,input_nc, ngf, batch_norm):
+    def __init__(self,input_nc, ngf, batch_norm = True):
         """
         This class down samples through the network
         :param input_nc: int
@@ -57,18 +57,25 @@ class up_sample(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, ngf, ndf, input_nc, output_nc, batch_norm):
+    def __init__(self, ngf,input_nc, output_nc, batch_norm = False):
+        """
+        This is a Generator architecture with skip connections.
+        :param ngf: number of filters
+        :param input_nc: number of input channels
+        :param output_nc: number of output channels
+        :param batch_norm: use batch norm
+        """
         super(Generator, self).__init__()
-
         self.down_stack = nn.Sequential(
-                        down_sample(input_nc, ngf, batch_norm),
-                        down_sample(ngf, ngf * 2, batch_norm),
-                        down_sample(ngf * 2, ngf * 4, batch_norm),
-                        down_sample(ngf * 4, ngf * 8, batch_norm),
-                        down_sample(ngf * 8, ngf * 8, batch_norm),
-                        down_sample(ngf * 8, ngf * 8, batch_norm),
-                        down_sample(ngf * 8, ngf * 8, batch_norm),
-                        down_sample(ngf * 8, ngf * 8, batch_norm))
+                        down_sample(input_nc, ngf, batch_norm=False),
+                        down_sample(ngf, ngf * 2),
+                        down_sample(ngf * 2, ngf * 4),
+                        down_sample(ngf * 4, ngf * 8),
+                        down_sample(ngf * 8, ngf * 8),
+                        down_sample(ngf * 8, ngf * 8),
+                        down_sample(ngf * 8, ngf * 8),
+                        down_sample(ngf * 8, ngf * 8,)
+        )
 
         self.up_stack = nn.Sequential(
                         up_sample(ngf * 8, ngf * 8, drop_out= True),
@@ -80,7 +87,7 @@ class Generator(nn.Module):
                         up_sample(ngf * 2, ngf))
 
         self.last = nn.Sequential(
-            nn.ConvTranspose2d(ngf, output_nc, stride=2, padding='same'),
+            nn.ConvTranspose2d(ngf, output_nc,kernel_size=4, stride=2, padding='same'),
             nn.Tanh()
         )
     def forward(self, x):
@@ -99,5 +106,58 @@ class Generator(nn.Module):
 
         return self.last(x)
 
+class NLayerDiscriminator(nn.Module):
+    """Defines a PatchGAN discriminator, adopted from Junyaz
+       Added comments and cleaned code.
+       The result of a PatchGAN is a feature map which tells us if each patch is True or False
+    """
+
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d):
+        """Construct a PatchGAN discriminator
+        Parameters:
+            input_nc (int)  -- the number of channels in input images
+            ndf (int)       -- the number of filters in the last conv layer
+            n_layers (int)  -- the number of conv layers in the discriminator
+            norm_layer      -- normalization layer
+        """
+        super(NLayerDiscriminator, self).__init__()
+        "Start a sequence with conv layer + leakyRelu"
+        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=4, stride=2, padding=1),
+                    nn.LeakyReLU(0.2, True)]
+        "Initialise constants that will control the number of filters in each layer as we progress"
+        nf_mult = 1
+        nf_mult_prev = 1
+
+        "Depending on n_layers we will build a discriminator"
+        for n in range(1, n_layers):
+            "Update Values to have correct channel sizes"
+            nf_mult_prev = nf_mult
+            nf_mult = min(2 ** n, 8)    #limit max channels size to ndf * 8.
+
+            sequence += [
+                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=4, stride=2, padding=1),
+                norm_layer(ndf * nf_mult),
+                nn.LeakyReLU(0.2, True)
+            ]
+
+
+        nf_mult_prev = nf_mult
+        nf_mult = min(2 ** n_layers, 8)
+
+        "Second Last Layer"
+        sequence += [
+            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=4, stride=1, padding=1),
+            norm_layer(ndf * nf_mult),
+            nn.LeakyReLU(0.2, True)
+        ]
+
+        "Final layer"
+        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=4, stride=1, padding=1)]  # output 1 channel prediction map
+
+        self.model = nn.Sequential(*sequence)
+
+    def forward(self, input):
+        """forward through discriminator"""
+        return self.model(input)
 
 
